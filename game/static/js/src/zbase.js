fb262b36204031923a0448fe50e808fb13528c7f
dingwann <catchskatecc@gmail.com>
2024-07-07 17:18:42 +0800

    write playground

--- game/static/js/src/zbase.js
+++ game/static/js/src/zbase.js
@@ -1,8 +1,8 @@
-class WcGame {
+export class WcGame {
     constructor(id) {
         this.id = id;
         this.$wc_game = $(`#${id}`);
-        this.menu = new WcGameMenu(this);
+        // this.menu = new WcGameMenu(this);
         this.playground = new WcGamePlayground(this);
 
         this.start();
