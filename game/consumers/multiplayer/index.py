23c4af3c2a65aaef48df298b70df9e29924d2f08
dingwann <catchskatecc@gmail.com>
2024-07-13 19:13:06 +0800

    implement thrift match service

--- game/consumers/multiplayer/index.py
+++ game/consumers/multiplayer/index.py
@@ -3,6 +3,16 @@ import json
 from django.conf import settings
 from django.core.cache import cache
 
+from thrift import Thrift
+from thrift.transport import TSocket
+from thrift.transport import TTransport
+from thrift.protocol import TBinaryProtocol
+
+from match_system.src.match_server.match_service import Match
+from game.models.player.player import Player
+from channels.db import database_sync_to_async
+
+
 class MultiPlayer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = None
@@ -31,27 +41,45 @@ class MultiPlayer(AsyncWebsocketConsumer):
         await self.channel_layer.group_add(self.room_name, self.channel_name)
 
     async def disconnect(self, close_code):
-        print('disconnect')
-        await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        if self.room_name:
+            print('disconnect')
+            await self.channel_layer.group_discard(self.room_name, self.channel_name)
 
     async def create_player(self, data):
-        player = cache.get(self.room_name)
-        player.append({
-            'uuid': data['uuid'],
-            'username': data['username'],
-            'photo': data['photo']
-        })
-        cache.set(self.room_name, player, 3600) # 60 minutes后过期
+        self.room_name = None
+        self.uuid = data['uuid']
+        # Make socket
+        transport = TSocket.TSocket('127.0.0.1', 9090)
 
-        await self.channel_layer.group_send(self.room_name, {
-            'type': 'group_send_event',
-            'event': 'create_player',
-            'uuid': data['uuid'],
-            'username': data['username'],
-            'photo': data['photo']
-        })
+        # Buffering is critical. Raw sockets are very slow
+        transport = TTransport.TBufferedTransport(transport)
+
+        # Wrap in a protocol
+        protocol = TBinaryProtocol.TBinaryProtocol(transport)
+
+        # Create a client to use the protocol encoder
+        client = Match.Client(protocol)
+
+        def db_get_player():
+            return Player.objects.get(user__username=data['username'])
+        
+        player = await database_sync_to_async(db_get_player)()
+
+        # Connect!
+        transport.open()
+
+        client.add_player(player.score, data['uuid'], data['username'], data['photo'], self.channel_name)
+
+        # Close!
+        transport.close()
+
+
 
     async def group_send_event(self, data):
+        if not self.room_name:
+            keys = cache.keys('*%s*' % (self.uuid))
+            if keys:
+                self.room_name = keys[0]
         await self.send(text_data = json.dumps(data))
 
 
